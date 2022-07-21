777edb52037388d2be325830ea84d0df53cf0af3
TuhinBar <tuhinbr99@gmail.com>
2022-07-21 08:59:32 +0530

    Update fibonacci.py

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -6,13 +6,4 @@ def getFib(n):
     return n + getFib(n-1)
 
 # driver code
-
 print(getFib(5))
-
-# Alternate solution
-def getFib(n):
-    if n==1:
-        return 1
-    if n==2:
-        return 1
-    return getFib(n-1) + getFib(n-2)
